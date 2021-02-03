0cd4930c20b124147824f46c00647d81cb5d1d70
Alyssa Aldridge <ana81@georgetown.edu>
2021-02-03 17:39:58 -0500

    ask user for an input

--- game.py
+++ game.py
@@ -1,1 +1,55 @@
-print("Rock, paper, scissors, shoot!")
+# game.py
+
+# introduction to game
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+# asking for user input
+
+
+x = input("Please choose either 'rock', 'paper', or 'scissors':")
+
+# print(x)
+print("You chose:", x)
+
+
+# can print many things in a string as long as they are seperated by commas
+# print("you chose:",x,"another string",x)
+
+# string concat:
+# print("you chose:" + "other string here")
+
+# string inerpolation / format string usage
+# print(f"you chose: {x}")
+
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("You chose: 'rock'")
+
+
+# simulating a computer input
+print("The computer chose: 'paper'")
+
+# determining who won
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")
