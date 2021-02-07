416402ab10e3cc23c52a17d023da578e3a684a30
Alyssa Aldridge <ana81@georgetown.edu>
2021-02-07 12:17:39 -0500

    update user name
    fixed error in user input for player name
    Also added an error trap in case the user inputed a capital letter

--- game.py
+++ game.py
@@ -1,64 +1,52 @@
 # game.py
 
 # importing random function
-
 import random
-
 import os
-
-
 import dotenv
 from dotenv import load_dotenv
 
-# alternative: from random import choice 
-#from dotenv import load_dotenv # go get the function we need
-#load_dotenv() # invoke the function
-#USER_NAME = os.getenv("USER_NAME", default="Player One") 
+
+
+load_dotenv() # invoke the function
+
 
 # introduction to game
 print("-------------------")
-# print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+USER_NAME = os.getenv("USER_NAME", default="Player One")
 
-USER_NAME = os.getenv("USER_NAME")
-
-print(f"PLAYER: '{USER_NAME}'")
-#> "Jon Snow"
+print(f"PLAYER:'{USER_NAME}'")
 print("-------------------")
 
 # asking for user input
 
 
 user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
+user_choice = user_choice.lower()
 
 # print(x)
 print("You chose:", user_choice)
 
+# NOTES from class: different ways to print a string 
+        # can print many things in a string as long as they are seperated by commas
+        # print("you chose:",x,"another string",x)
 
-# can print many things in a string as long as they are seperated by commas
-# print("you chose:",x,"another string",x)
+        # string concat:
+        # print("you chose:" + "other string here")
 
-# string concat:
-# print("you chose:" + "other string here")
-
-# string inerpolation / format string usage
-# print(f"you chose: {x}")
+        # string inerpolation / format string usage
+        # print(f"you chose: {x}")
 
 
 
 # simulating a computer input
-
-#computer_choice = "paper"
-
-
 options = ['rock', 'paper', 'scissors']
-
 
 # validate the user selection
 #
 # stop the program (not try to determine the winner)
 # ... if the user choice is invalid
 
-user_choice.lower()
 
 if user_choice in options:
     pass
@@ -66,22 +54,14 @@ else:
         print("OOPS, please choose a valid option and try again")
         exit()
 
-
         
 computer_choice = random.choice(options)
-# alternative: computer_choice = choice(options)
-
+#NOTES from class
+        # alternative: computer_choice = choice(options)
 
 print(f"The computer chose: {computer_choice}")
 
-
-
-
-
 # determining who won
-
-
-
 #single equal sign assigns value to variable. the way it is evaluated as saying what is on the right, and store it in the left
 # equality operations. is what is on the left equal to what is on the right? true or false
 
